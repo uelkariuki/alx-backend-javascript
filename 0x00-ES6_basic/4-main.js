8789ea35e9faf51dda4fedcb5e979a340593fd2a
uelkariuki <cratzuel@gmail.com>
2023-11-27 20:12:45 +0300

    Return the number of arguments passed to it using the rest parameter syntax

---
+++ 0x00-ES6_basic/4-main.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/extensions */
+/* eslint-disable no-console */
+import returnHowManyArguments from './4-rest-parameter.js';
+
+console.log(returnHowManyArguments('one'));
+console.log(returnHowManyArguments('one', 'two', 3, '4th'));
---
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -0,0 +1,4 @@
+/* eslint-disable no-console */
+export default function returnHowManyArguments(...args) {
+  return args.length;
+}
