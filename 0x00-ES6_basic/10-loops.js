0127bc8a508e690f0d420cff8a79d8e5ac3e7231
uelkariuki <cratzuel@gmail.com>
2023-11-28 00:03:19 +0300

    Rewrite the function appendToEachArrayValue to use ES6’s for...of operator

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-	const result = [];
-	for (const value of array) {
-	  result.push(appendString + value);
-	}
-
-	return result;
+  const result = [];
+  // eslint-disable-next-line no-restricted-syntax
+  for (const value of array) {
+    result.push(appendString + value);
   }
+
+  return result;
+}
--- 0x00-ES6_basic/10-main.js
+++ 0x00-ES6_basic/10-main.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/extensions */
+/* eslint-disable no-console */
 import appendToEachArrayValue from './10-loops.js';
 
 console.log(appendToEachArrayValue(['appended', 'fixed', 'displayed'], 'correctly-'));
