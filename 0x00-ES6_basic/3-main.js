0b746f22b1970ba04535f5e748531c1f7a188fb9
uelkariuki <cratzuel@gmail.com>
2023-11-27 19:31:27 +0300

    Define default parameter values for the function parameters

---
+++ 0x00-ES6_basic/3-main.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-console */
+// eslint-disable-next-line import/extensions
+import getSumOfHoods from './3-default-parameter.js';
+
+console.log(getSumOfHoods(34));
+console.log(getSumOfHoods(34, 3));
+console.log(getSumOfHoods(34, 3, 4));
