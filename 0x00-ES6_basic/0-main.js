1d63f6db133417ab000b2611c1a398c675523319
uelkariuki <cratzuel@gmail.com>
2023-11-27 15:43:24 +0300

    Fix ESLint errors

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,3 +1,5 @@
+// eslint-disable-next-line import/extensions
 import { taskFirst, taskNext } from './0-constants.js';
 
+// eslint-disable-next-line no-console
 console.log(`${taskFirst()} ${taskNext()}`);
