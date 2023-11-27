cb6669651beba742daa23d5ea06657ceb27d192c
uelkariuki <cratzuel@gmail.com>
2023-11-27 20:29:59 +0300

    Use spread syntax to concatenate 2 arrays and each character of a string

---
+++ 0x00-ES6_basic/5-main.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-console */
+/* eslint-disable import/extensions */
+import concatArrays from './5-spread-operator.js';
+
+console.log(concatArrays(['a', 'b'], ['c', 'd'], 'Hello'));
