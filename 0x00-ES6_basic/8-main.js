89652849017e5c1efbaabb37c4007aef7cf9ded7
uelkariuki <cratzuel@gmail.com>
2023-11-27 22:25:19 +0300

    Rewrite the getBudgetForCurrentYear function to use ES6 computed property names on the budget object

---
+++ 0x00-ES6_basic/8-main.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-console */
+/* eslint-disable import/extensions */
+import getBudgetForCurrentYear from './8-getBudgetCurrentYear.js';
+
+console.log(getBudgetForCurrentYear(2100, 5200, 1090));
