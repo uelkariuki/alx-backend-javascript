84d5caf6515f5cbb20afa6dbd78b09f5d729f66d
uelkariuki <cratzuel@gmail.com>
2023-11-27 17:55:17 +0300

    modify the variables inside the function taskBlock so  the variables aren’t overwritten inside the conditional block

---
+++ 0x00-ES6_basic/1-main.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-console */
+// eslint-disable-next-line import/extensions
+import taskBlock from './1-block-scoped.js';
+
+console.log(taskBlock(true));
+console.log(taskBlock(false));
