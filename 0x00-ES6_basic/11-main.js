2e648cd1777cc61f9c1585e90b6f3099c398ec04
uelkariuki <cratzuel@gmail.com>
2023-11-28 01:10:51 +0300

    Implement iterator on the function createEmployeesObject

---
+++ 0x00-ES6_basic/11-main.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/extensions */
+import createEmployeesObject from './11-createEmployeesObject.js';
+
+// eslint-disable-next-line no-console
+console.log(createEmployeesObject('Software', ['Bob', 'Sylvie']));
