2f5ee186f7f71dc30d8d61993d70d780465d5ba1
uelkariuki <cratzuel@gmail.com>
2023-11-27 21:29:38 +0300

    Rewrite the return statement to use a template literal

---
+++ 0x00-ES6_basic/6-main.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/extensions */
+import getSanFranciscoDescription from './6-string-interpolation.js';
+
+// eslint-disable-next-line no-console
+console.log(getSanFranciscoDescription());
