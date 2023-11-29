b87ef05d9a592a80e76ca252b7d578ecca67015e
uelkariuki <cratzuel@gmail.com>
2023-11-29 17:22:00 +0300

    Add function handleProfileSignup that handles multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,11 +1,17 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  // eslint-disable-next-line max-len
-  const results = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
-  return results.map((result) => ({
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
-  }));
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+  const results = [];
+  return Promise.allSettled(promises)
+    .then((result) => {
+      result.map(({ status, value, reason }) => (
+        results.push({
+          status,
+          value: status === 'rejected' ? reason.toString() : value,
+        })
+      ));
+      return results;
+    });
 }
