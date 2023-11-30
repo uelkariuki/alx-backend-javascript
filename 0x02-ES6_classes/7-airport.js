16c8bfd6d1da8f56704336a0203ba1369059dc61
uelkariuki <cratzuel@gmail.com>
2023-12-01 01:23:31 +0300

    Implement a class named Airport and change the default string description of the class

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-underscore-dangle */
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'string') {
+      throw new Error('Name must be a string');
+    }
+    if (typeof code !== 'string') {
+      throw new Error('Code must be a string');
+    }
+
+    this._name = name;
+    this._code = code;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
