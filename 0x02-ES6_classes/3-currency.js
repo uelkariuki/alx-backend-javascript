2ba9c4b6e936ae620d54df31a89b6058dfa9bbd1
uelkariuki <cratzuel@gmail.com>
2023-11-30 18:39:32 +0300

    Implement a class named Currency and added the method displayFullCurrency

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-underscore-dangle */
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw new Error('code must be a string');
+    }
+    if (typeof name !== 'string') {
+      throw new Error('code must be a string');
+    }
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
