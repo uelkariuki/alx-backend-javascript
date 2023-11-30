23a60db80c694aa174c03e1ebfe55549e9a7eb6a
uelkariuki <cratzuel@gmail.com>
2023-12-01 00:23:26 +0300

    Implement a class named SkyHighBuilding that extends from Building and override the method evacuationWarningMessage

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-underscore-dangle */
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+
+    if (typeof floors !== 'number') {
+      throw new Error('floors must be a number');
+    }
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(floors) {
+    this._floors = floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors.`;
+  }
+}
