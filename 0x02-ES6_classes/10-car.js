eb29622a7e4e0eab61b1bee0d2a629c1fe32d0f1
uelkariuki <cratzuel@gmail.com>
2023-12-01 03:23:22 +0300

    Implement a class named Car and Add a method named cloneCar that returns a new object of the class

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-underscore-dangle */
+// let brand = Symbol('brand');
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    // return new this.constructor(this._brand, this._motor, this._color);
+    return new this.constructor();
+  }
+}
