e55682e3eb4b37932c0f710b141399d8e5686121
uelkariuki <cratzuel@gmail.com>
2023-12-04 23:21:52 +0300

    Create a function named getListStudents that returns an array of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,5 +1,5 @@
 export default function getListStudents() {
-  let listOfObjects = [
+  const listOfObjects = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
