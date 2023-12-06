b0222e768858d524a029ce3ca9717bee9d28db75
uelkariuki <cratzuel@gmail.com>
2023-12-06 03:07:40 +0300

    Track within the weakMap the number of times queryAPI is called for each endpoint

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,12 +1,14 @@
 export const weakMap = new WeakMap();
 
+const MAX_CALLS = 5;
+
 export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
     weakMap.set(endpoint, 0);
   }
   const count = weakMap.get(endpoint);
-  if (count >= 5) {
+  weakMap.set(endpoint, count + 1);
+  if (count >= MAX_CALLS) {
     throw new Error('Endpoint load is high');
   }
-  weakMap.set(endpoint, count + 1);
 }
