002ffbc19d287367c6406a0c0990607206e817b9
uelkariuki <cratzuel@gmail.com>
2024-01-30 20:22:00 +0300

    Chore: Create a small HTTP server using Node's HTTP module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('Hello Holberton School!');
+  res.end();
+}).listen(1245);
+module.exports = app;
