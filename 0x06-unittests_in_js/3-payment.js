b0ac0c475f6bac6861c475c7d5a80bb51db76abb
uelkariuki <cratzuel@gmail.com>
2024-02-01 17:40:19 +0300

    Implement the usage of Spies

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,4 +1,7 @@
+const Utils = require('./utils');
+
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
   let result = Utils.calculateNumber('SUM', totalAmount, totalShipping);
   console.log(`The total is: ${result}`);
 }
+module.exports = sendPaymentRequestToApi;
